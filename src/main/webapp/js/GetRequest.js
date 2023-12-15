ae512c0f59fcaf75e7a49192ec1d95dd7df61104
Pablo Vera <pablovera14@yahoo.com.ar>
2023-12-15 15:27:30 -0300

    Cambios en los estilos

--- src/main/webapp/js/GetRequest.js
+++ src/main/webapp/js/GetRequest.js
@@ -1,104 +1,86 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-    const booksCards = document.getElementById("booksCards");
-    const books = [];
-
-
-    function loadBookList() {
+document.addEventListener("DOMContentLoaded", function(){
+    const bookCards = document.getElementById("bookCards");
+    const books =[];
+    
+    function loadBookList(){
         fetch("/app/books?action=getAll")
-                .then(response => response.json())
-
-                .then(data => {
-                    console.log(data);                    
-                    data.forEach(book => {
+                .then(response=> response.json())
+                .then(data =>{
+                    data.forEach(book =>{
                         books.push(book);
-                        booksCards.innerHTML += ` <div class="card" style="width: 18rem;">
-                            <div class="card-body">
-                                <img src="${book.image}" class="card-img-top" alt="">
-                                <h5 class="card-title">${book.nombre}</h5>
-                                <h6 class="card-subtitle mb-2 text-body-secondary">${book.autor}</h6>
-                                <p class="card-text">${book.sinopsis}</p>
-                                <a href="#" class="card-link">Precio: ${book.precio}</a>
-                                <a href="#" class="card-link">Copias disponibles: ${book.copias}</a>
-                                
+                        bookCards.innerHTML += `
+                            <div class="col-md-3 mb-4 ident" data-book-id="${book.idlibros}">
+                                <div class="card h-100 animate-hover-card">
+                                    <img src="data:image/jpeg;base64,${book.imagenBase64}" class="card-img-top h-50  img-fluid cardImage" alt="Imagen Portada de Libro">
+                                    <div class="card-body ">
+                                        <h5 class="card-tittle">${book.nombre}</h5>
+                                        <p class="card-text">${book.sinopsis}</p>
+                                    </div>
+                                </div>
                             </div>
-                        </div>`;
+                        `;
                     });
                 });
     }
-
-    booksCards.addEventListener("click", function (e) {
-        const clickedCard = e.target.closest(".ident");
-        if (!clickedCard) {
-            return;
-        }
-
-        const bookId = clickedCard.dataset.bookId;
-
-        fetch(`/app/books?action=getDetails&id=${bookId}`)
-                .then(response => response.json())
-                .then(bookDetails => {
-                    const queryParams = new URLSearchParam({
-                        id: bookDetails.idlibros
-                    });
-
-                    window.location.href = `/app/pages/bookDetails.html?${queryParams.toString()}`;
-                })
-                .catch(error => console.error("Error en la solicitud GET", error));
-
-    });
     
     function filterBooks(palabra){
-        const librosFiltrados = books.filter(book=>{
-           return book.nombre.toLowerCase().includes(palabra.toLowerCase()); 
+        const librosFiltrados = books.filter( book=>{
+            return book.nombre.toLowerCase().includes(palabra.toLowerCase());
         });
         
-        bookCards.innerHTML= "";
+        bookCards.innerHTML = "";
         
-        librosFiltrados.forEach(book=>{
+         librosFiltrados.forEach(book => {
             const card = document.createElement("div");
             card.className = "col-md-3 mb-4 ident";
             card.setAttribute("data-book-id", book.idlibros);
             card.innerHTML = `
-                <div class="card h-100 animate-hover-card">
-                    <img src="data:image/jpg;base64,${book.imagenBase64}" class="card-img-top h-75 " alt="Imagen de portada">
-                    <div class="card-body">
-                        <h5 class="card-title">${book.nombre}</h5>
-                        <p class="card-text"> ${book.sinopsis}</p>
-                    </div>
-                </div>    
-
-            `;
+            <div class="card h-100 animate-hover-card">
+                <img src="data:image/jpeg;base64,${book.imagenBase64}" class="card-img-top h-75" alt="imagen de portada">
+                <div class="card-body">
+                    <h5 class="card-title">${book.nombre}</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        `;
             bookCards.appendChild(card);
         });
-        
-    };
+    }
     
+    //Evento que lanza la funcion de filtrar y agregar tarjetas de libros.
     const searchForm = document.querySelector("form[role='search']");
     searchForm.addEventListener("submit", function(e){
        e.preventDefault();
-       const searchTerm = searchForm.querySelector("input"[type='search']).value;
-       filterBooks(searcTerm);
+       const searchTerm = searchForm.querySelector("input[type='search']").value;
+       filterBooks(searchTerm);
     });
     
-    bookCards.addEventListener("click", function(e){        
-       const clickedCard = e.target.closest(".ident");
+    //evento para mostrar detalles de libros
+    
+    bookCards.addEventListener("click", function(e){
+        const clickedCard = e.target.closest(".ident");
+        if(!clickedCard){
+            return;
+        }
         
-    })
+        const bookId = clickedCard.dataset.bookId;
+        
+        fetch(`/app/books?action=getDetails&id=${bookId}`)
+                .then(response => response.json())
+                .then(bookDetails =>{
+                    const queryParams =  new URLSearchParams({
+                       id : bookDetails.idlibros
+                    });
+                    
+                    window.location.href = `/app/pages/bookDetails.html?${queryParams.toString()}`;
+                })
+                .catch(error => console.error("Error en la solicitud GET:", error));
+    });
+    
+    loadBookList();
     
     
-    
-    
-
-    loadBookList();    
-
 });
-
-
-
-
-
-
 
 
 
