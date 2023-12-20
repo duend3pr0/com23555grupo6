736ec2591877f00de0e855dd40daa84a1eb6a81e
Pablo Vera <pablovera14@yahoo.com.ar>
2023-12-20 20:41:52 -0300

    Funcionando completamente

--- src/main/webapp/js/PostRequest.js
+++ src/main/webapp/js/PostRequest.js
@@ -1,22 +1,71 @@
-document.addEventListener("DOMContentLoaded",function(){
-    const imagen = document.getElementById("imagen");
+document.addEventListener("DOMContentLoaded", function(){
+    
+    const addBookForm = document.getElementById("addBookForm");
+    const parrafoAlerta = document.createElement("P");
+    const tituloElement = document.getElementById("nombre");
+    const autorElement = document.getElementById("autor");
+    const cantPaginasElement = document.getElementById("cantPaginas");
+    const copiasElement = document.getElementById("copias");
+    const sinopsisElement = document.getElementById("sinopsis");
+    const precioElement = document.getElementById("precio");
+    
+    const imageElement = document.getElementById("imagen");
     const imagenPreview = document.getElementById("imagenPreview");
     const imagenContainer = document.getElementById("imagenContainer");
     
-    imagen.addEventListener("change", function(){
-        const selectedImage = imagen.files[0];
+    addBookForm.addEventListener("submit", function(e){
+        
+        e.preventDefault();
+        
+        const datos = new FormData();
+        
+        datos.append("action","add");
+        datos.append("nombre",tituloElement.value);
+        datos.append("autor",autorElement.value);
+        datos.append("cantPaginas",cantPaginasElement.value);
+        datos.append("copias",copiasElement.value);
+        datos.append("sinopsis",sinopsisElement.value);
+        datos.append("precio",precioElement.value);
+        datos.append("imagen",imageElement.files[0]);
+        
+        fetch("/app/books", {
+            method: "POST",
+            body: datos
+        })
+                .then(response => response.json())
+                .then(data=> {
+                    parrafoAlerta.textContent = data.message;
+                    addBookForm.appendChild(parrafoAlerta);
+                    
+                    setTimeout(()=>{
+                        parrafoAlerta.remove();
+                        tituloElement.value = "";
+                        autorElement.value = "";
+                        cantPaginasElement.value = "";
+                        copiasElement.value = "";
+                        sinopsisElement.value = "";
+                        precioElement.value = "";
+                        imageElement.value = "";
+                        imagenContainer.classList.add("d-none");
+                    },3000)
+                });
+    });
+    
+    imageElement.addEventListener("change", function(){
+        const selectedImage = imageElement.files[0];
         
         if(selectedImage){
             const reader = new FileReader();
             reader.onload = function(e){
                 imagenPreview.src = e.target.result;
-                imagenContainer.classList.remove("d-none");               
+                imagenContainer.classList.remove("d-none");
             };
             
-            reader.readAsDataURL((selectedImage));
+            reader.readAsDataURL(selectedImage);
         }else{
-                imagenPreview.src = "";
-            }       
+            imagenPreview.src= "";
+        }
     });
- });
- 
+    
+});
+
