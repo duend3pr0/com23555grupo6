02fe5a5bc08f8bd3cbca968124332778b888291a
Pablo Vera <pablovera14@yahoo.com.ar>
2023-12-20 20:41:12 -0300

    Funcionando completamente

--- src/main/java/data/Conexion.java
+++ src/main/java/data/Conexion.java
@@ -1,14 +1,16 @@
 package data;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
 
-
 public class Conexion {
-   private static final String JDBC_URL = "jdbc:mysql://localhost:3306/libros?useSSL=false&useTimeZone=UTC&allowPublicKeyRetrieval=true";
-   private static final String JDBC_USER = "root";
-   private static final String JDBC_PASS = "Florista1909"; 
+    private static final String JDBC_URL ="jdbc:mysql://localhost:3306/libros?useSSL=false&useTimeZone=true&serverTimeZone=UTC&allowPublicKeyRetrieval=true";
+    private static final String JDBC_USER="root";
+    private static final String JDBC_PASS="Florista1909";
     
     public static DataSource getDataSource(){
         BasicDataSource ds = new BasicDataSource();
@@ -16,36 +18,29 @@ public class Conexion {
         ds.setUsername(JDBC_USER);
         ds.setPassword(JDBC_PASS);
         
-        ds.setInitialSize(100); 
-        System.out.println(ds);
-        System.out.println("dsStatus");
-        return   ds;
+        ds.setInitialSize(100);
+        return ds;
     }
     
     public static Connection getConexion()throws SQLException{
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("Conexión a la base de datos exitosa.");
         }catch(ClassNotFoundException ex){
             ex.printStackTrace(System.out);
-            System.out.println("Error al cargar el driver de la base de datos.");
         }
-            return getDataSource().getConnection();
-        }   
-  
-    public static void close(Connection con, Statement st, ResultSet rs) {
-        try {
-            if (rs != null) {
-                rs.close();
-            }
-            if (st != null) {
-                st.close();
-            }
-            if (con != null) {
-                con.close();
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace(System.out); // o manejo adecuado de la excepción
-        }
+        
+        return getDataSource().getConnection();
+    }
+    
+    public static void close(ResultSet rs) throws SQLException{
+        rs.close();
+    }
+    
+    public static void close(Statement st) throws SQLException{
+        st.close();
+    }
+    
+    public static void close(Connection cn) throws SQLException{
+        cn.close();
     }
 }
